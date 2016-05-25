a4c17d681fd3f03f14cfec3bcab7093d2457690f
Erik Pukinskis <erik@snowedin.net>
2016-05-24 21:46:42 -0700

    make isASelector a little more generous

--- element.js
+++ element.js
@@ -107,10 +107,14 @@ module.exports = library.export(
       el.attributes.style = style
     }
 
-    var whitelist = /^(a|body|button|canvas|div|form|h1|h2|h3|head|html|iframe|img|input|li|meta|p|script|span|style|textarea|ul)?(\.[^.]+)*$/
+    var whitelist = /^(a|body|button|canvas|div|form|h1|h2|h3|head|html|iframe|img|input|li|meta|p|script|span|style|textarea|ul)?(\[|\.)/
 
     function isASelector(string) {
-      return !!string.match(whitelist)
+      var itIs = !!string.match(whitelist)
+      if (!itIs) {
+        debugger
+      }
+      return itIs
     }
 
     function addSelector(parsed, selector) {
